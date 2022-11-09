b74058745e509b4f53e5fcd08914475017420ac2
Benedict <benedictryan80@gmail.com>
2022-11-10 06:01:26 +0700

    enable transfer acceleration

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
 	swcMinify: true,
 	images: {
 		domains: [
-			`${process.env.S3_UPLOAD_BUCKET}.s3.amazonaws.com`,
+			`${process.env.S3_UPLOAD_BUCKET}.s3-accelerate.amazonaws.com`,
 			`${process.env.S3_UPLOAD_BUCKET}.s3.${process.env.S3_UPLOAD_REGION}.amazonaws.com`
 		]
 	}
