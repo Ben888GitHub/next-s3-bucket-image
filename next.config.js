af325203250a4fd3ea8d8496342bb32b4a20647f
Benedict <benedictryan80@gmail.com>
2022-11-10 04:05:50 +0700

    first commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+	reactStrictMode: true,
+	swcMinify: true,
+	images: {
+		domains: [
+			`${process.env.S3_UPLOAD_BUCKET}.s3.amazonaws.com`,
+			`${process.env.S3_UPLOAD_BUCKET}.s3.${process.env.S3_UPLOAD_REGION}.amazonaws.com`
+		]
+	}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
